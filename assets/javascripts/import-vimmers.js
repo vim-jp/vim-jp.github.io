9ea8ebac5d4cac9c1f4d04ad7853ed789face704
MURAOKA Taro <koron.kaoriya@gmail.com>
2013-03-24 20:14:24 +0900

    add shuffle and show all buttons

--- assets/javascripts/import-vimmers.js
+++ assets/javascripts/import-vimmers.js
@@ -3,6 +3,9 @@
 google.load("visualization", "1");
 $(function() {
 
+  var elementCache = [];
+  var limits = 50;
+
   function escapeHTML(str) {
     return str.replace('&', '&amp;')
       .replace('<', '&lt;')
@@ -55,124 +58,162 @@ $(function() {
     return rows;
   }
 
-  var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=0AvP2GcrHKTfudEdwWm85ajZtd0ltZjRUcVFPWHpzWHc&gid=0&pub=1');
-  query.send(function(response) {
-    if(response.isError()) {
-      return;
+  function toElement(getter) {
+    var name        = getter('Name');
+    var shortname   = getter('Short Name');
+    var image       = getter('Image');
+    var twitter     = getter('Twitter Account');
+    var github      = getter('Github Account');
+    var facebook    = getter('Facebook');
+    var googleplus  = getter('Google+');
+    var vimorg      = getter('www.vim.org');
+    var website     = getter('Website URL');
+    var description = getter('Description');
+
+    var items = [];
+
+    items.push([ 'li', { 'class': 'name' }, name + (shortname ? ' \u0028' + shortname + '\u0029' : '') ]);
+
+    if (twitter) {
+      var twitter_url = 'https://twitter.com/' + twitter;
+      if (!image) {
+        image = 'http://api.twitter.com/1/users/profile_image/'
+          + twitter + '.png&size=mini';
+      }
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': twitter_url }, [ 'img', { 'src': '/assets/images/icon-twitter.png', 'title': 'Twitter' } ] ]
+      ]);
     }
-    var data = response.getDataTable();
-    var colNames = (function() {
-      var colNames = {};
-      for (var i = 0, max = data.getNumberOfColumns(); i < max; i++) {
-        colNames[data.getColumnLabel(i)] = i;
-      }
-      return colNames;
-    })();
 
-    $('#vimmers-container').empty();
-    var rowMax = data.getNumberOfRows();
-    var shuffledRows = shuffleNumbers(rowMax);
-    for (var row = 0; row < rowMax; row++) {
-      var r = shuffledRows[row];
-      var name        = data.getFormattedValue(r, colNames['Name']);
-      var shortname   = data.getFormattedValue(r, colNames['Short Name']);
-      var image       = data.getFormattedValue(r, colNames['Image']);
-      var twitter     = data.getFormattedValue(r, colNames['Twitter Account']);
-      var github      = data.getFormattedValue(r, colNames['Github Account']);
-      var facebook    = data.getFormattedValue(r, colNames['Facebook']);
-      var googleplus  = data.getFormattedValue(r, colNames['Google+']);
-      var vimorg      = data.getFormattedValue(r, colNames['www.vim.org']);
-      var website     = data.getFormattedValue(r, colNames['Website URL']);
-      var description = data.getFormattedValue(r, colNames['Description']);
-
-      var items = [];
-
-      items.push([ 'li', { 'class': 'name' }, name + (shortname ? ' (' + shortname + ')' : '') ]);
-
-      if (twitter) {
-        var twitter_url = 'https://twitter.com/' + twitter;
-        if (!image) {
-          image = 'http://api.twitter.com/1/users/profile_image/'
-            + twitter + '.png&size=mini';
-        }
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': twitter_url }, [ 'img', { 'src': '/assets/images/icon-twitter.png', 'title': 'Twitter' } ] ]
-        ]);
-      }
-
-      if (github) {
-        var github_url = 'https://github.com/' + github;
-        if (!image) {
-          items.unshift([
-            'li', { 'class': 'faceicon ' },
-            [ 'img', { 'src': '/assets/images/icon-loading.gif', 'class': github } ]
-          ]);
-          (function(github) {
-            $.getJSON('https://api.github.com/users/' + github + '?callback=?', function(res) {
-              $('img.' + github).attr('src', res.data.avatar_url);
-            });
-          })(github);
-        }
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': github_url }, [ 'img', { 'src': '/assets/images/icon-github.png', 'title': 'GitHub' } ] ]
-        ]);
-      }
-
-      if (facebook) {
-        var facebook_url = 'https://www.facebook.com/' + facebook;
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': facebook_url }, [ 'img', { 'src': '/assets/images/icon-facebook.png', 'title': 'Facebook' } ] ]
-        ]);
-      }
-
-      if (googleplus) {
-        var googleplus_url = 'https://plus.google.com/' + googleplus
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': googleplus_url }, [ 'img', { 'src': '/assets/images/icon-googleplus.png', 'title': 'Google+' } ] ]
-        ]);
-      }
-
-      if (website) {
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': website }, [ 'img', { 'src': '/assets/images/icon-website.png', 'title': 'WebSite' } ] ]
-        ]);
-      }
-
-      if (vimorg) {
-        var vimorg_url = 'http://www.vim.org/account/profile.php?user_id=' + vimorg;
-        items.push([
-          'li', { 'class': 'link' },
-          [ 'a', { 'href': vimorg_url }, [ 'img', { 'src': '/assets/images/icon-vim.png', 'title': 'Vim' } ] ]
-        ]);
-      }
-
-      items.push([ 'li', { 'class': 'clear' } ]);
-
-      if (description) {
-        items.push([ 'li', { 'class': 'desc' } ]
-            .concat(to_jsonml(escapeHTML(description))));
-      }
-
-      if (image) {
+    if (github) {
+      var github_url = 'https://github.com/' + github;
+      if (!image) {
         items.unshift([
-          'li', { 'class': 'faceicon' },
-          [ 'img', { 'src': image } ]
+          'li', { 'class': 'faceicon ' },
+          [ 'img', { 'src': '/assets/images/icon-loading.gif', 'class': github } ]
         ]);
+        (function(github) {
+          $.getJSON('https://api.github.com/users/' + github + '?callback=?', function(res) {
+            $('img.' + github).attr('src', res.data.avatar_url);
+          });
+        })(github);
       }
-
-      $.jqml([
-        'div', { 'class': 'person', },
-        [ 'ul' ].concat(items)
-      ]).appendTo('#vimmers-container');
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': github_url }, [ 'img', { 'src': '/assets/images/icon-github.png', 'title': 'GitHub' } ] ]
+      ]);
     }
-    $('#vimmers-container').masonry({
-      isAnimated: true,
-      isResizable: true
+
+    if (facebook) {
+      var facebook_url = 'https://www.facebook.com/' + facebook;
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': facebook_url }, [ 'img', { 'src': '/assets/images/icon-facebook.png', 'title': 'Facebook' } ] ]
+      ]);
+    }
+
+    if (googleplus) {
+      var googleplus_url = 'https://plus.google.com/' + googleplus
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': googleplus_url }, [ 'img', { 'src': '/assets/images/icon-googleplus.png', 'title': 'Google+' } ] ]
+      ]);
+    }
+
+    if (website) {
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': website }, [ 'img', { 'src': '/assets/images/icon-website.png', 'title': 'WebSite' } ] ]
+      ]);
+    }
+
+    if (vimorg) {
+      var vimorg_url = 'http://www.vim.org/account/profile.php?user_id=' + vimorg;
+      items.push([
+        'li', { 'class': 'link' },
+        [ 'a', { 'href': vimorg_url }, [ 'img', { 'src': '/assets/images/icon-vim.png', 'title': 'Vim' } ] ]
+      ]);
+    }
+
+    items.push([ 'li', { 'class': 'clear' } ]);
+
+    if (description) {
+      items.push([ 'li', { 'class': 'desc' } ]
+          .concat(to_jsonml(escapeHTML(description))));
+    }
+
+    if (image) {
+      items.unshift([
+        'li', { 'class': 'faceicon' },
+        [ 'img', { 'src': image } ]
+      ]);
+    }
+
+    return $.jqml([
+      'div', { 'class': 'person', },
+      [ 'ul' ].concat(items)
+    ]);
+  }
+
+  function refreshVimmers(first) {
+    var container = $('#vimmers-container');
+    container.empty();
+    var order = shuffleNumbers(elementCache.length);
+    var max = Math.min(order.length, limits);
+    for (var i = 0; i < max; ++i) {
+      container.append(elementCache[order[i]]);
+    }
+    if (first) {
+      container.masonry({
+        isAnimated: true,
+        isResizable: true
+      });
+    } else {
+      console.log('reload');
+      container.masonry('reload');
+    }
+  }
+
+  function loadVimmers(end_cb) {
+    elementCache = [];
+    var query = new google.visualization.Query('https://spreadsheets.google.com/tq?key=0AvP2GcrHKTfudEdwWm85ajZtd0ltZjRUcVFPWHpzWHc&gid=0&pub=1');
+    query.send(function(response) {
+      if(response.isError()) {
+        return;
+      }
+      var data = response.getDataTable();
+      var colNames = (function() {
+        var colNames = {};
+        for (var i = 0, max = data.getNumberOfColumns(); i < max; i++) {
+          colNames[data.getColumnLabel(i)] = i;
+        }
+        return colNames;
+      })();
+
+      $('#vimmers-container').empty();
+      var rowMax = data.getNumberOfRows();
+      for (var row = 0; row < rowMax; row++) {
+        var element = toElement(function(name) {
+          return data.getFormattedValue(row, colNames[name]);
+        });
+        elementCache.push(element);
+      }
+      refreshVimmers(true);
+      if (end_cb) {
+        end_cb();
+      }
+    });
+  }
+
+  loadVimmers(function() {
+    $('#vimmers-shuffle').click(function() {
+      refreshVimmers(false);
+    });
+    $('#vimmers-showall').click(function() {
+      limits = elementCache.length;
+      refreshVimmers(false);
+      $('#vimmers-showall').hide();
     });
   });
 });
