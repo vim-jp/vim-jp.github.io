1b1e8c49fab68056c86a20d4d221bac8f590ff62
Yukihiro Nakadaira <yukihiro.nakadaira@gmail.com>
2013-09-07 19:31:27 +0900

    gc

--- scripts/vimdoc-ja-update-html.sh
+++ scripts/vimdoc-ja-update-html.sh
@@ -9,7 +9,6 @@ git clone git@github.com:vim-jp/vimdoc-ja.git
 cd vimdoc-ja
 MASTER_TIME=`git log -1 --pretty=format:%ct origin/master`
 GHPAGES_TIME=`git log -1 --pretty=format:%ct origin/gh-pages`
-echo "$MASTER_TIME $GHPAGES_TIME"
 if [ $MASTER_TIME -gt $GHPAGES_TIME ] ; then
   git checkout -b devel origin/devel
   make htmlbatch
