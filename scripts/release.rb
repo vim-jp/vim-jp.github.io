68ab4aead24289a2d315b8ebb5e24ea911fb1eed
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-08-31 15:57:05 +0900

    release Windows binaries (7.4.430 +kaoriya).  move and separate downloads section into _data/ directory

--- scripts/release.rb
+++ scripts/release.rb
@@ -5,10 +5,9 @@ require 'date'
 require 'yaml'
 
 root = File::expand_path("#{File::dirname(__FILE__)}/..")
-conf = YAML.load_file("#{root}/_config.yml")
+conf = YAML.load_file("#{root}/_data/downloads.yml")
 
-if !ARGV.empty? && conf['downloads'].has_key?(ARGV[0])
-  rel = conf['downloads'][ARGV[0]]
+if !ARGV.empty? and rel = conf[ARGV[0]]
   ver = rel['version'].gsub(/\s/, '')
   file = "_posts/release/#{Date.today.to_s}-#{ARGV[0]}-#{ver}.md"
   open("#{root}/#{file}", "wb") {|f|
@@ -32,5 +31,5 @@ EOS
 else
   puts "usage: #{File.basename(__FILE__)} {release-name}"
   puts "--------"
-  puts conf['downloads'].keys.join("\n")
+  puts conf.keys.join("\n")
 end
