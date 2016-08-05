824f2118b282fd14582f64e35f09785e7335daea
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-08-05 18:32:24 +0900

    use ruby 2.2.5

--- eyecatch.rb
+++ eyecatch.rb
@@ -2,7 +2,6 @@ serve 'bundle exec jekyll serve'
 port 4000
 
 before_build {
-  run 'rbenv versions'
-  run 'rbenv install --list'
+  run 'rbenv local 2.2.5'
   run 'bundle install --path vendor --binstubs'
 }
