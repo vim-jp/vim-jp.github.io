a95aa4f621ee77415a0ff01ef30ffebfd3435de5
MURAOKA Taro <koron.kaoriya@gmail.com>
2016-08-05 18:41:51 +0900

    fix

--- eyecatch.rb
+++ eyecatch.rb
@@ -2,6 +2,7 @@ serve 'bundle exec jekyll serve'
 port 4000
 
 before_build {
-  run 'rbenv global 2.2.5'
+  run 'rbenv versions'
+  run 'rbenv local 2.2.5'
   run 'bundle install --path vendor --binstubs'
 }
