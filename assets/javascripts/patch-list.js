3d8a24c9550141f46a8423fb0842559da735098f
K.Takata <kentkt@csc.jp>
2017-03-16 21:52:23 +0900

    Refine patch list (Issue #241)
    * Decode character references using `$('<div/>').html(str).text()`.
    * Remove duplicated description.
    * Insert a line break before 'Solution:'.

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -3,9 +3,10 @@ $(function() {
 
   $.getJSON("//vim-jp.herokuapp.com/patches/json?callback=?", function(items) {
     $.each(items, function() {
+      var desc = $('<div/>').html(this.description).text().replace(/^[^\n]*\n\n/, '').replace(/\n(?=Solution:)/, '<br/>')
       $('<li/>').append(
         $('<a/>').attr({'href': this.link, 'target': '_blank'}).text("Patch " + this.title)
-      ).append($('<br/>')).append($('<span/>').text(this.description)).appendTo('#patches-list');
+      ).append($('<br/>')).append($('<span/>').html(desc)).appendTo('#patches-list');
     });
   });
 });
