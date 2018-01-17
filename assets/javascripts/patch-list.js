0224bab39347d8d4285f8e18e482bae57f22fe52
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 12:30:06 +0900

    fix patch-list

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -3,10 +3,10 @@ $(function() {
 
   $.getJSON("https://api.github.com/repos/vim/vim/commits?callback=?", function(items) {
     $.each(items.data, function() {
-      var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n(.*)/).shift();
+      var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n(.*)/);
       $('<li/>').append(
-        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token[0])
-      ).append($('<br/>')).append($('<span/>').html(token[1] || '')).appendTo('#patches-list');
+        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token[1])
+      ).append($('<br/>')).append($('<span/>').html(token[2] || '')).appendTo('#patches-list');
     });
   });
 });
