76450a87d81224d9ef9a435c96fb9b0306b9a4d5
K.Takata <kentkt@csc.jp>
2018-01-17 19:03:52 +0900

    Fix patch-list
    * Escape symbols properly.
    * Adjust format. Use the same format as before.
    * Don't show more than 10 patches.

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -2,11 +2,15 @@ $(function() {
   'use strict';
 
   $.getJSON("https://api.github.com/repos/vim/vim/commits?callback=?", function(items) {
-    $.each(items.data, function() {
-      var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n([\s\S]*)/);
+    $.each(items.data, function(index) {
+      if (index == 10) {
+        return false;
+      }
+      var token = this.commit.message.split(/\n\n/, 2);
+      var desc = $('<div/>').text(token[1] || '').html().replace(/\n(?=Solution:)/, '<br/>');
       $('<li/>').append(
-        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token ? token[1] : this.commit.message)
-      ).append($('<br/>')).append($('<span/>').html(token ? token[2].replace(/\n/g, '<br/>')||'' : '')).appendTo('#patches-list');
+        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token[0].replace(/^patch (\d+\.\d+\.\d+).*$/, 'Patch $1'))
+      ).append($('<br/>')).append($('<span/>').html(desc)).appendTo('#patches-list');
     });
   });
 });
