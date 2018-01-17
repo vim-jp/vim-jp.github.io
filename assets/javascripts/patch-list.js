7e50bdb2110fb2833bedab2c0dfbbb676cab8c6a
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 12:28:40 +0900

    fix patch-list

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -3,7 +3,7 @@ $(function() {
 
   $.getJSON("https://api.github.com/repos/vim/vim/commits?callback=?", function(items) {
     $.each(items.data, function() {
-      var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n(.*)/).shift8)
+      var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n(.*)/).shift();
       $('<li/>').append(
         $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token[0])
       ).append($('<br/>')).append($('<span/>').html(token[1] || '')).appendTo('#patches-list');
