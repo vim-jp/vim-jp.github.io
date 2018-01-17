fdd8c29cf37d0339d906e74731749498477d9f61
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 12:39:25 +0900

    fix patch-list
    make line-break

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -6,7 +6,7 @@ $(function() {
       var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n([\s\S]*)/);
       $('<li/>').append(
         $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token ? token[1] : this.commit.message)
-      ).append($('<br/>')).append($('<span/>').html(token ? token[2]||'' : '')).appendTo('#patches-list');
+      ).append($('<br/>')).append($('<span/>').html(token ? token[2].replace(/\n/g, '<br/>')||'' : '')).appendTo('#patches-list');
     });
   });
 });
