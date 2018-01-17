fb13c1ebee61187f5631beac224e005f01555438
Yasuhiro Matsumoto <mattn.jp@gmail.com>
2018-01-17 12:32:01 +0900

    fix patch-list

--- assets/javascripts/patch-list.js
+++ assets/javascripts/patch-list.js
@@ -5,8 +5,8 @@ $(function() {
     $.each(items.data, function() {
       var token = this.commit.message.replace(/\n\n+/g, '\n').match(/^([^\n]+)\n(.*)/);
       $('<li/>').append(
-        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token[1])
-      ).append($('<br/>')).append($('<span/>').html(token[2] || '')).appendTo('#patches-list');
+        $('<a/>').attr({'href': this.html_url, 'target': '_blank'}).text(token ? token[1] : this.commit.message)
+      ).append($('<br/>')).append($('<span/>').html(token ? token[2]||'' : '')).appendTo('#patches-list');
     });
   });
 });
